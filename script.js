1a4682c1e3080638a4da2042b1ab237869f61980
Casenrique <carloshenriquesouza.eng@gmail.com>
2022-10-03 13:48:01 -0300

    Adicionando exercício 1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+//Exercício 1
+
 const frutas = ["laranja", "limão", "uva"];
+
+const laranja = document.getElementById("fruta-1")
+laranja.innerHTML += frutas[0]
+
+const limao = document.getElementById("fruta-2")
+limao.innerHTML += frutas[1]
+
+const uva = document.getElementById("fruta-3")
+uva.innerHTML += frutas[2]
+
+//Acredito que dê para fazer uma função variando o índice [i]
