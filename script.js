527d61e71db64bdc83a4b2d0cb840b0e44a6e706
Casenrique <carloshenriquesouza.eng@gmail.com>
2022-10-03 15:03:30 -0300

    Adicionando os 3 exercícios antes do feedback

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ limao.innerHTML += frutas[1]
 const uva = document.getElementById("fruta-3")
 uva.innerHTML += frutas[2]
 
+// let adicionando = null
+
+// const adicionaFruta = () => {
+//     for (let i = 0; i < frutas.length; i++)
+//         adicionando = document.getElementById(`frutas-${i}`)
+//         adicionando.innerHTML += frutas[i]
+// }
+
+// adicionaFruta()
+
 //Acredito que dê para fazer uma função variando o índice [i]
+
+//Exercício 2 Exercício 3
+
+const frutaAdicionada = () => {
+    let entradaFruta = document.getElementById("fruta");
+    console.log(entradaFruta.value)
+    frutas.push(entradaFruta.value)
+    console.log(frutas)
+    const frutaUsuario = document.getElementById("fruta-4")
+    frutaUsuario.innerHTML += entradaFruta.value
+    entradaFruta.value = ""
+}
+
